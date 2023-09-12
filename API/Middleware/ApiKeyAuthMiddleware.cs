d67f6c0a0081a19eea44035eb5289dc2cd8b40c2
Duong <ltson@theodorealexander.com>
2023-09-12 15:16:34 +0700

    add logg

--- API/Middleware/ApiKeyAuthMiddleware.cs
+++ API/Middleware/ApiKeyAuthMiddleware.cs
@@ -42,7 +42,7 @@ namespace API.Middleware
             }
             catch (Exception ex)
             {
-                // _logger.LogError(ex, ex.Message);
+                _logger.LogError(ex, ex.Message);
                 // context.Response.ContentType = "application/json";
                 // context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
